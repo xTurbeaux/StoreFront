b96a397b78ae611e44b20adbd893f821daae8ee5
xTurbeaux <standingbytocopy@gmail.com>
2020-09-04 11:09:53 -0700

    reworked metadata

--- AAronsAmmoShack/AAronsAmmoShack.DATA.EF/Metadata/DBAmmoShack.cs
+++ AAronsAmmoShack/AAronsAmmoShack.DATA.EF/Metadata/DBAmmoShack.cs
@@ -3,11 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AAronsAmmoShack.DATA.EF.Metadata
 {
-    class DBAmmoShack
+
+    #region Ammo Metadata
+
+    public class AmmoMetadata
+    {
+        [Required(ErrorMessage = "*Name of Ammo is Required*")]
+        [StringLength(20, ErrorMessage = "Ammo name must be 15 characters or less")]
+        [Display(Name = "Ammo")]
+        public string AmmoName { get; set; }
+
+        [UIHint("MultilineText")]
+        [DisplayFormat(NullDisplayText= "X")]
+        public string AmmoDescription { get; set; }
+
+        [DisplayAttribute(Name = "Class Active")]
+        public bool IsAvailable { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "value must be a valid number, 0 or larger")]
+        [DisplayFormat(DataFormatString = "{0:c}", NullDisplayText = "[NA]")]
+        public decimal Price { get; set; }
+
+        [DisplayAttribute(Name = "Class Active")]
+        public bool IsTracer { get; set; }
+
+        [DisplayAttribute(Name = "Class Active")]
+        public bool IsSubsonic { get; set; }
+
+        [Required(ErrorMessage = "Book status is required")]
+        public int MuzzleVelocity { get; set; }
+
+        [Display(Name = "Cover")]
+        public string ProductImage { get; set; }
+    }
+
+    [MetadataType(typeof(AmmoMetadata))]
+    public partial class Ammo
     {
 
     }
+
+    #endregion
+
+
 }
---
+++ AAronsAmmoShack/AAronsAmmoShack.UI.MVC/Controllers/ManufacturersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AAronsAmmoShack.DATA.EF;
+
+namespace AAronsAmmoShack.UI.MVC.Controllers
+{
+    public class ManufacturersController : Controller
+    {
+        private DBAmmoShackEntities db = new DBAmmoShackEntities();
+
+        // GET: Manufacturers
+        public ActionResult Index()
+        {
+            return View(db.Manufacturers1.ToList());
+        }
+
+        // GET: Manufacturers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturers manufacturers = db.Manufacturers1.Find(id);
+            if (manufacturers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturers);
+        }
+
+        // GET: Manufacturers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manufacturers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ManufacturerID,City,State,Country,Name")] Manufacturers manufacturers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Manufacturers1.Add(manufacturers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(manufacturers);
+        }
+
+        // GET: Manufacturers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturers manufacturers = db.Manufacturers1.Find(id);
+            if (manufacturers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturers);
+        }
+
+        // POST: Manufacturers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ManufacturerID,City,State,Country,Name")] Manufacturers manufacturers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(manufacturers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(manufacturers);
+        }
+
+        // GET: Manufacturers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturers manufacturers = db.Manufacturers1.Find(id);
+            if (manufacturers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturers);
+        }
+
+        // POST: Manufacturers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Manufacturers manufacturers = db.Manufacturers1.Find(id);
+            db.Manufacturers1.Remove(manufacturers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
